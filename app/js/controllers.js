eab2c3ced134ff5ea2c1cde8f99ee559e0058c00
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-08 15:59:03 +0200

    Implemented navigation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,3 +10,16 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function NavCtrl($scope, $location, $rootScope) {
+    $scope.views = [{path: '/view1', title: "View 1"},
+                    {path: '/view2', title: "View 2"}];
+
+    $rootScope.$watch(function() {return $location.path(); }, function() {
+        $scope.currentPage = $location.path();
+    });
+
+    $scope.activeNav = function(view) {
+        return view.path == $scope.currentPage ? "active" : "";
+    };
+}
