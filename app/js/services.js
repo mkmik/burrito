afa6c758a26315e7ef54db5b591667b5646f0011
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-11 13:42:20 +0200

    Removed pervasive reference to $rootScope in tablesManager

--- app/js/services.js
+++ app/js/services.js
@@ -24,24 +24,25 @@ angular.module('myApp.services', []).
     }).factory('tablesManager', function($rootScope, Tables, Table) {
         function TablesManager() {
             $rootScope.tables = [];
-            this.tables = $rootScope.tables
             Tables.bind($rootScope, 'tables');
+
+            this.tables = $rootScope.tables;
         }
 
         TablesManager.prototype.addTable = function() {
-            $rootScope.tables.push(new Table($rootScope.tables.length + 1));
+            this.tables.push(new Table(this.tables.length + 1));
             setTimeout(function() {$('#tables tr:last input:first').focus()}, 100);
         }
 
         TablesManager.prototype.removeTable = function(team) {
-            for (var i = 0, ii = $rootScope.tables.length; i < ii; i++) {
-                if (team === $rootScope.tables[i]) {
-                    $rootScope.tables.splice(i, 1);
+            for (var i = 0, ii = this.tables.length; i < ii; i++) {
+                if (team === this.tables[i]) {
+                    this.tables.splice(i, 1);
                 }
             }
             // renumber
-            for (var i = 0, ii = $rootScope.tables.length; i < ii; i++) {
-                $rootScope.tables[i].number = i+1;
+            for (var i = 0, ii = this.tables.length; i < ii; i++) {
+                this.tables[i].number = i+1;
             }
         }
 
