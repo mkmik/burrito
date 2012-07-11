cf8cbba4b55e13a658d29c7390bf911d378b2b9e
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-11 13:26:28 +0200

    Added basic drop functionality

--- app/js/directives.js
+++ app/js/directives.js
@@ -11,6 +11,8 @@ angular.module('myApp.directives', []).
   }]).directive('draggableTeam', function() {
       return {
           link: function(scope, element, attrs) {
+              $(element).data('draggable-team', scope.$eval(attrs.draggableTeam));
+
               $(element).draggable({scope: 'team',
                                     handle: '.icon-move',
                                     revert: true,
@@ -20,6 +22,14 @@ angular.module('myApp.directives', []).
                                     zIndex: 200,
                                     containment: '#tables',
                                     });
+
+              $(element).droppable({drop: handleDrop,
+                                    scope: 'team'
+                                   });
+
+              function handleDrop(event, ui) {
+                  console.log("dropped", ui.draggable.data('draggable-team'), "over", $(element).data('draggable-team'));
+              }
           }
       };
   });
