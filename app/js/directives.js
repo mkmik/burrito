0b0d0edd6a8f79a56f1b89268569f35d0b72a588
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-11 13:11:25 +0200

    Implemented draggable teams

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,18 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).directive('draggableTeam', function() {
+      return {
+          link: function(scope, element, attrs) {
+              $(element).draggable({scope: 'team',
+                                    handle: '.icon-move',
+                                    revert: true,
+                                    revertDuration: 200,
+                                    cursor: 'move',
+                                    snap: true,
+                                    zIndex: 200,
+                                    containment: '#tables',
+                                    });
+          }
+      };
+  });
